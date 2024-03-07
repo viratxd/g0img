80f4d3f1d70319a209b7b7d0d49e9dedba65ad79
Aoi Tanaka <aoitanaka.0523@gmail.com>
2024-03-07 18:26:58 +0100

    feat : add login & logout function with auth0

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,11 +1,16 @@
-import './App.css'
+import { useAuth0 } from "@auth0/auth0-react";
+import "./App.css";
+import LoginButton from "./components/LoginButton";
+import LogoutButton from "./components/LogoutButton";
 
 function App() {
-
+  const { isAuthenticated } = useAuth0();
   return (
     <>
+      Image Search
+      {isAuthenticated ? <LogoutButton /> : <LoginButton />}
     </>
-  )
+  );
 }
 
-export default App
+export default App;

f7711fc3fabc6b3594acca068784e8f59dcaca5c
Aoi Tanaka <aoitanaka.0523@gmail.com>
2024-03-08 17:07:34 +0100

    feat : add a component to show result

---
+++ client/src/components/ShowResult.tsx
@@ -0,0 +1,44 @@
+import { IImage } from "../models/IImage";
+import { getImages } from "../services/imageService";
+
+interface IShowResultProps {
+  searchTime: string;
+  correctedQuery: string;
+  images: IImage[];
+  isLoading: boolean;
+}
+
+export const ShowResult = ({
+  searchTime,
+  correctedQuery,
+  images,
+  isLoading,
+}: IShowResultProps) => {
+  return (
+    <>
+      {isLoading ? (
+        <span>loading</span>
+      ) : (
+        <section>
+          {searchTime ? <p>{searchTime}sec</p> : <></>}
+          {correctedQuery ? (
+            <p>
+              Did you mean
+              <a onClick={() => getImages(correctedQuery)}>
+                {correctedQuery}
+              </a>{" "}
+              ?
+            </p>
+          ) : (
+            <></>
+          )}
+          {images?.map((image) => (
+            <figure>
+              <img src={image.link} alt={image.title} />
+            </figure>
+          ))}
+        </section>
+      )}
+    </>
+  );
+};

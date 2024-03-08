2dba9c7f26ceb2503cc8b3cdd1d66b8ca0f2cff7
Aoi Tanaka <aoitanaka.0523@gmail.com>
2024-03-08 12:33:12 +0100

    feat : add a function to fetch data from API

---
+++ client/src/models/IImage.ts
@@ -0,0 +1,4 @@
+export interface IImage {
+    link: string;
+    title: string;
+}
--- client/src/pages/ImageSearch.tsx
+++ client/src/pages/ImageSearch.tsx
@@ -1,11 +1,42 @@
-import LogoutButton from "../components/LogoutButton"
+import axios from "axios";
+import LogoutButton from "../components/LogoutButton";
+import { useEffect, useState } from "react";
+import { IImage } from "../models/IImage";
 
 export const ImageSearch = () => {
-    
-    return (
-        <>
-            Image Search Page
-            <LogoutButton />
-        </>
-    )
-}
+  const [images, setImages] = useState<IImage[]>();
+  const [searchTime, setSearchTime] = useState("");
+
+  useEffect(() => {
+    const getData = async () => {
+      const response = await axios.get(
+        `https://www.googleapis.com/customsearch/v1?key=${
+          import.meta.env.VITE_GOOGLE_API_KEY
+        }&cx=${
+          import.meta.env.VITE_GOOGLE_SEARCH_ENGINE_ID
+        }&q=panda&num=10&searchType=image`
+      );
+      const data = await response.data;
+      setImages(data.items);
+      setSearchTime(data.searchInformation.formattedSearchTime);
+      console.log(data);
+    };
+    getData();
+  }, []);
+  console.log(searchTime);
+
+  return (
+    <>
+      <h1>Image Search Page</h1>
+      <div>
+        <p>{searchTime}sec</p>
+        {images?.map((image) => (
+          <figure>
+            <img src={image.link} alt={image.title} />
+          </figure>
+        ))}
+      </div>
+      <LogoutButton />
+    </>
+  );
+};

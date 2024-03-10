f1ec13b9dd7580dd615988e2225697e35355127a
Aoi Tanaka <aoitanaka.0523@gmail.com>
2024-03-10 14:02:41 +0100

    feat : styling for favorite page

--- client/src/pages/Favorite.tsx
+++ client/src/pages/Favorite.tsx
@@ -5,15 +5,14 @@ export const Favorite = () => {
   const { likedImages } = useContext(LikedImagesContext);
 
   return (
-    <div className="result-images">
-      {likedImages?.map((image) => (
-        <figure className="image">
-          <button>
-            <span className="material-symbols-outlined">favorite</span>
-          </button>
-          <img src={image.image.link} alt={image.image.title} />
-        </figure>
-      ))}
+    <div className="favorite">
+      <div className="favorite-images">
+        {likedImages?.map((image) => (
+          <figure className="image">
+            <img src={image.image.link} alt={image.image.title} />
+          </figure>
+        ))}
+      </div>
     </div>
   );
 };

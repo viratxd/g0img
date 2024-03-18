cf42105967b19a3d2fec4b51f6d7a3b758d0ff2b
Aoi Tanaka <aoitanaka.0523@gmail.com>
2024-03-18 21:45:42 +0100

    feat : save data i json file

--- server/server.js
+++ server/server.js
@@ -1,18 +1,23 @@
 const express = require("express");
 const cors = require("cors");
-const fs = require("fs");
+const fs = require("fs").promises;
 const { favoriteSchema } = require("./schemas/favorite.schema");
 const { userSchema } = require("./schemas/user.schema");
 
 const app = express();
 
-const users = [];
+let users = [];
+
+const writeFile = async () => {
+  await fs.writeFile("users.json", JSON.stringify(users, null, 1));
+};
 
 app.use(cors());
 app.use(express.json());
 
 app.get("/api/users", (req, res) => {
   res.status(200).json(users);
+  console.log("Users data loaded successfully");
 });
 
 app.post("/api/users", (req, res) => {
@@ -25,8 +30,14 @@ app.post("/api/users", (req, res) => {
   if (!existingUser) {
     const newUser = { user: req.body.userName, favoriteImages: [] };
     users.push(newUser);
-    res.status(201).json(req.body);
-    console.log("User successfully added");
+    try {
+      writeFile();
+      res.status(201).json(req.body);
+      console.log("User successfully added");
+    } catch (error) {
+      console.error("Error writing to file:", error);
+      res.status(500).json({ error: "Error writing to file" });
+    }
   } else {
     console.log("User already exists");
   }
@@ -36,11 +47,27 @@ app.put("/api/users", (req, res) => {
   const user = users.find((user) => user.user == req.body.userName);
   if (user) {
     user.favoriteImages = req.body.imageData;
+    try {
+      writeFile();
+      res.status(200).json(req.body.imageData);
+    } catch (error) {
+      console.error("Error writing to file:", error);
+      res.status(500).json({ error: "Error writing to file" });
+    }
   } else {
     console.log("User not found");
   }
-
-  res.status(200).json(req.body.imageData);
 });
+
+const getDataFromFile = async () => {
+  try {
+    const fileData = await fs.readFile("users.json");
+    users = JSON.parse(fileData);
+    console.log("Users data loaded successfully");
+  } catch (error) {
+    console.error("Error reading file:", error);
+  }
+};
+getDataFromFile();
 
 app.listen(3000, () => console.log("Server is up and running..."));

6a5cf9b56baa3cb3b32dff2d32f42e879131b798
Aoi Tanaka <aoitanaka.0523@gmail.com>
2024-03-20 23:18:30 +0100

    feat . styling for not found page

--- client/src/pages/NotFound.tsx
+++ client/src/pages/NotFound.tsx
@@ -1,7 +1,10 @@
+import { NavLink } from "react-router-dom"
+
 export const NotFound = () => {
     return (
-        <>
-            Not Found
-        </>
+        <div className="not-found">
+            <h1>Sorry, we can't find that page.</h1>
+            <NavLink to={"/"}>Back to Home 🏠</NavLink>
+        </div>
     )
 }

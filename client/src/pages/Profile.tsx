ee739ca71ca2911b19b8121051e2978913e4567e
Aoi Tanaka <aoitanaka.0523@gmail.com>
2025-04-23 17:09:20 +0200

    Fix error

--- client/src/pages/Profile.tsx
+++ client/src/pages/Profile.tsx
@@ -1,4 +1,4 @@
-import { useContext, useEffect, useState } from "react";
+import { useContext, useState } from "react";
 import { UserContext } from "../contexts/UserContext";
 
 export const Profile = () => {
